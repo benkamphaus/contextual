1c1a4b8e969252114384836924749f1a3074fb28
Ben Kamphaus <ben.kamphaus@gmail.com>
2019-06-17 15:34:28 -0600

    update project deps

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   know their context"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
-                 [org.clojure/clojurescript "0.0-3196"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.3-SNAPSHOT"]]
-                   :plugins [[lein-cljsbuild "1.0.5"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
+                   :plugins [[lein-cljsbuild "1.1.7"]
                              [com.cemerick/clojurescript.test "0.3.3"]]
                    :aliases {"test-all" ["do" "test," "cljsbuild" "test"]}
                    :cljsbuild {:test-commands {"all" ["phantomjs" "target/test-runner.js"]}
                                :builds [{:source-paths ["src" "test"]
                                          :compiler {:output-to "target/test-runner.js"
                                                     :optimizations :whitespace
-                                                    :pretty-print true}}]}}}
-  )
+                                                    :pretty-print true}}]}}})
+  
 
